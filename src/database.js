57f1dc1878544c343353d5ffe1bd01d8da1da86e
LDonGithub <ldonoso@gmail.cl>
2023-01-08 18:46:26 -0300

    Conexion con BD

---
+++ src/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
+
+const URI = 'mongodb://127.0.0.1/mern-tasks'
+
+mongoose.connect(URI)
+.then(db => console.log('DB is connected'))
+.catch(err => console.error(err));
+
+module.exports = mongoose;
