39a714010d963dedc57562b64189139283143698
LDonGithub <ldonoso@gmail.cl>
2023-01-08 00:22:39 -0300

    Agr. morgan + conf. middlewars

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 //settings
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
 
 //middlewares
+app.use(morgan('dev'));
+app.use(express.json());
 
 //routes
 
 //static files
 
 //starting server
-app.listen(4000, () => {
-    console.log('Server on port 4000');
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 })
