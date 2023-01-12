1da1b1834004b739a7f27825357adea0a15d2100
LDonGithub <ldonoso@gmail.cl>
2023-01-12 02:34:51 -0300

    Conexion formulario con base de datos

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,44 @@
 import React, {Component} from "react";
 
 class App extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            title: '',
+            description: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.addTask = this.addTask.bind(this);
+    }
+
+    addTask(e){
+        fetch('api/tasks', {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                M.toast({html: 'Tarea Guardada'});
+                this.setState({title: '', description: ''});
+            })
+            .catch(err => console.error(err));
+
+        e.preventDefault();
+    }
+
+    handleChange(e){
+        const {name, value} = e.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
     render(){
         return (
             <div className="App">
@@ -17,15 +55,15 @@ class App extends Component {
                         <div class="col s5">
                             <div class="card">
                                 <div class="card-content">
-                                    <form>
+                                    <form onSubmit={this.addTask}>
                                         <div class="row">
                                             <div class="input-field col s12">
-                                                <input type="text" placeholder="Titulo Tarea"></input>
+                                                <input name="title" type="text" placeholder="Titulo Tarea" value={this.state.title} onChange={this.handleChange}></input>
                                             </div>
                                         </div>
                                         <div class="row">
                                             <div class="input-field col s12">
-                                             <textarea id="textarea2" class="materialize-textarea" placeholder="Descripcion Tarea"></textarea>
+                                             <textarea name="description" id="textarea2" class="materialize-textarea" placeholder="Descripcion Tarea" value={this.state.description} onChange={this.handleChange}></textarea>
                                             </div>
                                         </div>
                                         <button class="btn #5e35b1 deep-purple darken-1" type="submit" name="action">Enviar
@@ -37,7 +75,6 @@ class App extends Component {
                         </div>
                     </div>
                 </div>
-
 
 
             </div>
